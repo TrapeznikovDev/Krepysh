c23696c03a1eb4f480edae4c745b6a572365e5c7
Dmitriy <trapeznikov.dev@gmail.com>
2022-06-01 14:17:03 +0300

    Kids profile done

--- app/src/main/java/com/example/newkrepysh/ui/profile/KidsProfileFragment.kt
+++ app/src/main/java/com/example/newkrepysh/ui/profile/KidsProfileFragment.kt
@@ -7,14 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.example.newkrepysh.ComponentManager
 import com.example.newkrepysh.R
 import com.example.newkrepysh.databinding.FragmentKidsProfileBinding
 import com.example.newkrepysh.databinding.KidsItemLayoutBinding
+import com.example.newkrepysh.entities.kidprofile.DataObject
 import com.example.newkrepysh.factory.ViewModelFactory
 import com.example.newkrepysh.local.Repository
 import com.example.newkrepysh.ui.home.HomeViewModel
+import com.example.newkrepysh.ui.profile.recycler.KidsProfileAdapter
+import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.Entry
@@ -45,11 +49,27 @@ class KidsProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         ComponentManager.instance.getFragmentComponent(this).inject(this)
+
+        val data: MutableList<DataObject> = ArrayList()
+
+        data.add(DataObject("абонементы"))
+        data.add(DataObject("расписание"))
+        data.add(DataObject("история тренировок"))
+        data.add(DataObject("достижения"))
+        data.add(DataObject("анкета"))
+
         _binding = FragmentKidsProfileBinding.inflate(inflater, container, false)
         args = this.arguments?.getInt("child_id") ?: 0
         argsUser = this.arguments?.getInt("user_id") ?: 0
+
+
         binding.performBackBtn.setOnClickListener {
             findNavController().popBackStack()
+        }
+        with(binding){
+            kidProfileRecycler.hasFixedSize()
+            kidProfileRecycler.adapter = KidsProfileAdapter(data)
+            kidProfileRecycler.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
         }
         return binding.root
 
@@ -64,11 +84,70 @@ class KidsProfileFragment : Fragment() {
             Glide.with(this)
                 .load(it.user?.media?.firstOrNull()?.url?.topThumb)
                 .into(kid_profile_photo)
+
             binding.placeInSchoolNumber.text = it.user?.schoolRating.toString()
             binding.placeInCityNumber.text = it.user?.cityRating.toString()
             binding.placeInCountryNumber.text = it.user?.countryRating.toString()
 
+            initeLineChart()
+
+            initeProgressBar()
 
         }
     }
+
+    private fun initeProgressBar() {
+        val progress = 40
+        with(binding) {
+            workingOutProgressBar.setProgress(progress, false)
+
+        }
+    }
+
+
+    private fun initeLineChart() {
+        val entries = ArrayList<Entry>()
+
+        entries.add(Entry(1f, 10f))
+        entries.add(Entry(2f, 2f))
+        entries.add(Entry(3f, 7f))
+        entries.add(Entry(4f, 20f))
+        entries.add(Entry(5f, 16f))
+
+        val set = LineDataSet(entries, " ")
+        set.mode = LineDataSet.Mode.CUBIC_BEZIER
+        set.lineWidth = 3F
+
+        set.color = requireActivity().resources.getColor(R.color.text_orange)
+        set.setDrawCircles(false)
+        set.setDrawFilled(true)
+        set.fillColor = requireActivity().resources.getColor(R.color.text_orange)
+        set.highLightColor = requireActivity().resources.getColor(R.color.text_orange)
+        set.fillAlpha = 20
+        set.setDrawValues(false)
+        val dat = LineData(set)
+        with(binding) {
+            lineChart.setDrawBorders(false)
+            val yaxis = lineChart.getAxis(YAxis.AxisDependency.LEFT)
+            val secondYaxis = lineChart.getAxis(YAxis.AxisDependency.RIGHT)
+            yaxis.setDrawAxisLine(false)
+            secondYaxis.setDrawAxisLine(false)
+            yaxis.setDrawLabels(false)
+            lineChart.minOffset = 0F
+            secondYaxis.setDrawLabels(false)
+            yaxis.isEnabled = false
+            secondYaxis.isEnabled = false
+            lineChart.xAxis.isEnabled = false
+            lineChart.axisLeft.setDrawGridLines(false)
+            lineChart.axisRight.setDrawGridLines(false)
+            lineChart.legend.isEnabled = false
+            lineChart.setDrawGridBackground(false)
+            lineChart.data = dat
+            lineChart.description.isEnabled = false
+            GlobalScope.launch(Dispatchers.Main) {
+                lineChart.invalidate()
+            }
+        }
+    }
+
 }
